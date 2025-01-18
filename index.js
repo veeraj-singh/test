ac54ba056dc4e8fbb0c20ff29baf4949000b55ed
veeraj-singh <veerbachche82@gmail.com>
2025-01-18 23:50:37 +0530

    init2

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv')
 const app = express();
-
-
+dotenv.config()
 app.use(bodyParser.json());
-
-
-const API_KEY = "341dd7f9fa57ee22a1bb6e4c13315f92a8467c24d67d84e35a41f9ade0d0721f";
-
 
 app.post("/api/zapier/invoice-responses", (req, res) => {
     const apiKey = req.headers["x-api-key"];
-    if (apiKey !== API_KEY) {
+    if (apiKey !== process.env.API_KEY) {
         return res.status(401).json({ error: "Unauthorized" });
     }
-
     const { Body, Date, Subject } = req.body;
-
     if (!Body || !Date || !Subject) {
         return res.status(400).json({ error: "Missing required fields in the payload" });
     }
-
     console.log("Webhook received:", {
         body: Body,
         date: Date,
         subject: Subject,
     });
-
     res.status(200).json({ status: "success", message: "Webhook processed successfully" });
 });
 
